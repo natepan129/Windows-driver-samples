d87f9ca43a8603dac36e16a47d85cc7489c66fb8
natepan129 <nate@owl3d.ai>
2025-10-31 17:48:35 +0900

    delete unused bat fiel

--- video/IndirectDisplay/IddSampleDriver/run_setupapi_install_admin.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Running SetupAPI installation as Administrator...
-powershell -ExecutionPolicy Bypass -Command "Start-Process powershell -Verb RunAs -ArgumentList '-ExecutionPolicy Bypass -NoExit -File install_with_setupapi.ps1'"
-
-
